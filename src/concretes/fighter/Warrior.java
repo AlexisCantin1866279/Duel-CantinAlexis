eab0dfae6a2f2b6493359a9b0ed4dbe54f141a5f
Alexis <1866279@cegep-ste-foy.qc.ca>
2019-09-24 15:31:06 -0400

    feat: validation aptitude de guerrier

--- src/concretes/fighter/Warrior.java
+++ src/concretes/fighter/Warrior.java
@@ -1,9 +1,23 @@
 package concretes.fighter;
 
-public class Warrior extends Fighter{
+import exceptions.fighter.WarriorIllegalSkillPoints;
+
+public class Warrior extends Fighter {
+
+	public static final int WARRIOR_SKILLS_CAP = 10;
 
 	public Warrior(String name, int strength, int dexterity, int intelligence, int concentration) {
 		super(name, strength, dexterity, intelligence, concentration);
+		validateWarriorSkills(strength, dexterity, intelligence, concentration);
+	}
+
+	private void validateWarriorSkills(int strength, int dexterity, int intelligence, int concentration) {
+		int dexterityDifference = dexterity + WARRIOR_SKILLS_CAP;
+		int intelligenceDifference = intelligence + WARRIOR_SKILLS_CAP;
+
+		if (strength < dexterityDifference || dexterityDifference < intelligenceDifference
+				|| intelligenceDifference < concentration)
+			throw new WarriorIllegalSkillPoints();
 	}
 
 }
---
+++ src/exceptions/fighter/WarriorIllegalSkillPoints.java
@@ -0,0 +1,6 @@
+package exceptions.fighter;
+
+@SuppressWarnings("serial")
+public class WarriorIllegalSkillPoints extends IllegalArgumentException{
+
+}
