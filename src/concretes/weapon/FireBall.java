a883049a2dc6ed32100f1f7ce2ab390a9c05b15c
Alexis <1866279@cegep-ste-foy.qc.ca>
2019-09-27 17:53:44 -0400

    docs: mise a jour du diagramme

--- src/concretes/weapon/FireBall.java
+++ src/concretes/weapon/FireBall.java
@@ -2,7 +2,7 @@ package concretes.weapon;
 
 public class FireBall extends Weapon {
 
-	public FireBall(int power, weaponType type) {
+	public FireBall(int power, weaponType type) { // demande pour pas que l'utilisateur face le type
 		super(power, type);
 	}
 
