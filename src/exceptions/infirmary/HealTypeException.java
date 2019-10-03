486d9da39bf39e98128f8cb02b80976deeedc403
Alexis <1866279@cegep-ste-foy.qc.ca>
2019-10-03 14:31:15 -0400

    style: supression des excetions non utilisees

--- src/exceptions/infirmary/HealTypeException.java
+++
@@ -1,6 +0,0 @@
-package exceptions.infirmary;
-
-@SuppressWarnings("serial")
-public class HealTypeException extends IllegalArgumentException {
-
-}
--- src/exceptions/weapon/IllegalAttack.java
+++
@@ -1,6 +0,0 @@
-package exceptions.weapon;
-
-@SuppressWarnings("serial")
-public class IllegalAttack extends RuntimeException {
-
-}
